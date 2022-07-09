50b4ebbb9de5dc53d31dabf8a41c72650d0deab0
Viskal <viskal.bayu@gmail.com>
2022-07-09 20:52:02 +0700

    update

---
+++ assets/AboutView.f482802e.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as s,a as e,b as a}from"./index.405ebd33.js";const _={},c={class:"about"},r=e("h1",null,[a(" About Me -> "),e("a",{target:"_blank",href:"https://viskalb.wordpress.com/"},"Click here!")],-1),n=[r];function i(l,d){return o(),s("div",c,n)}var h=t(_,[["render",i]]);export{h as default};
